57257439a9794a17b0331d7ec468c9f116aa72e1
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-03-08 15:04:04 -0800

    Update ScriptableObjectExample.cs

--- FunWithAttributes/Assets/Scripts/ScriptableObjectExample.cs
+++ FunWithAttributes/Assets/Scripts/ScriptableObjectExample.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 
+//this is an awesome way to create a new Scriptable Object in your project using the Create Menu!
 [CreateAssetMenu(fileName = "SO_Example", menuName ="ScriptableObjects/NewExample", order = 0)]
 public class ScriptableObjectExample : ScriptableObject
 {
